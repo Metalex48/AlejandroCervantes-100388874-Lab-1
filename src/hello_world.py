58863cd72740fe5442d0fcdb99d3e134c8e57fe8
Alejandro Cervantes <acervantesnass00@mylangara.ca>
2022-05-24 19:21:19 -0700

    my fourth commit

---
+++ src/hello_world.py
@@ -0,0 +1,26 @@
+print('Hello, world!')
+print('Welcome to computing for Data Analytics')
+
+x=12
+y=3
+print(x+y)
+
+d = 'this is a test'
+d.capitalize
+print(d)
+
+test_boolean = True
+print (test_boolean)
+
+#Here I tried to import some constants from another file 'using_contant.py'
+import using_constant
+print(f'the constant is {using_constant.PI}')
+
+x=(1,2,3)
+print(type(x))
+y=[1,2,3]
+print(type(y))
+
+# Here I tried to import sys.
+#import sys
+#print(dir(sys))
